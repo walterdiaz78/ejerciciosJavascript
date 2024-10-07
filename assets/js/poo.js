56bfefc9e09ce3e8bd9ca96459397a84e8b1ee28
walterdiaz78 <walterdiaz78@gmail.com>
2024-10-07 15:07:30 -0300

    Agrego metodo

--- assets/js/poo.js
+++ assets/js/poo.js
@@ -9,6 +9,11 @@ class animal {
         this.informacion = `<h1> - Hola, mi nombre es ${nombre}, soy un ${this.species} de raza ${this.raza}, soy de color ${color} y tengo ${edad} años de edad </h1>`
         
     }
+    //metodo
+    verInformacion(){
+        document.write(this.informacion + "<br>")
+    }
+    
 }
 
 //perro va a ser igual a un objeto de la clase animal
@@ -17,6 +22,10 @@ let gato = new animal("Mishi", "gato", "Siames", 3, "Blanco y negro")
 let pingüino = new animal("Tux", "pingüino", "no especificada", 4, "Negro y blanco")
 console.log(perro)
 
-document.write(perro.informacion + "<br>")
-document.write(gato.informacion + "<br>")
-document.write(pingüino.informacion + "<br>")
+// document.write(perro.informacion + "<br>")
+// document.write(gato.informacion + "<br>")
+// document.write(pingüino.informacion + "<br>")
+
+perro.verInformacion()
+gato.verInformacion()
+pingüino.verInformacion()  
