98a8561d2dbe59752708ff6c94e7d001fe5d4c33
walterdiaz78 <walterdiaz78@gmail.com>
2024-10-08 16:40:40 -0300

    Ejercicio 4 practicas de POO

--- assets/js/poo.js
+++ assets/js/poo.js
@@ -165,97 +165,178 @@
 // perro.setTamaño = "grande";
 // document.write(perro.getTamaño);
 
-//EJERCICIO 3
+// //EJERCICIO 3
 
-class Celular {
-  constructor(color, peso, tamaño, rdc, ram) {
-    this.color = color;
+// class Celular {
+//   constructor(color, peso, tamaño, rdc, ram) {
+//     this.color = color;
+//     this.peso = peso;
+//     this.tamaño = tamaño;
+//     this.resolucionDeCamara = rdc;
+//     this.memoriaRam = ram;
+//     this.encendido = false;
+//   }
+//   presionarBotonEncendido() {
+//     if (this.encendido == false) {
+//       alert("Celular prendido");
+//       this.encendido = true;
+//     } else {
+//       alert("El celula apagado");
+//       this.encendido = false;
+//     }
+//   }
+//   reiniciar() {
+//     if (this.encendido == true) {
+//       alert("Celular reiniciado");
+//     } else {
+//       alert("El celular esta apagado");
+//     }
+//   }
+//   tomarFoto() {
+//     alert(`foto tomada en resolucion de: ${this.resolucionDeCamara}`);
+//   }
+
+//   grabarVideo() {
+//     alert(`Grabando video en resolucion de: ${this.resolucionDeCamara}`);
+//   }
+
+//   mobileInfo() {
+//     return `
+//         Color: <b>${this.color}</b><br>
+//         Peso: <b>${this.peso}</b><br>
+//         Tamaño: <b>${this.tamaño}</b><br>
+//         resolucion de camara: <b>${this.resolucionDeCamara}</b><br>
+//         Memoria ram: <b>${this.memoriaRam}</b><br>
+//         `;
+//   }
+// }
+
+// class CelularAltaGama extends Celular{
+//     constructor(color, peso, tamaño, rdc, ram,rdce){
+//         super(color, peso, tamaño, rdc, ram)
+//         this.resolucionDeCamaraExtra = rdce
+// }
+//     grabarVideoLento(){
+//         alert("Grabando en camara lenta")
+//     }
+//     reconocimientoFacial(){
+//         alert("Reconociendo rostro")
+//     }
+
+//     infoAltaGama(){
+//         return this.mobileInfo() +
+//         `Resolucion de camara extra: <b>${this.resolucionDeCamaraExtra}</b>`
+//     }
+
+// }
+
+// const celular1 = new Celular("rojo", "150gr", "5'", "Hd", "1Gb");
+// const celular2 = new Celular("negro", "155gr", "5.4", "Full Hd", "2GB");
+// const celular3 = new Celular("blanco", "146gr", "5.9", "Full Hd", "2GB");
+
+// // celular1.presionarBotonEncendido()
+// // celular1.tomarFoto()
+// // celular1.grabarVideo()
+// // celular1.reiniciar()
+// // celular1.presionarBotonEncendido()
+
+// const celular4 = new CelularAltaGama("rojo", "130gr", "5.2'", "4K", "3Gb","Full HD")
+// const celular5 = new CelularAltaGama("negro", "140gr", "6'", "4K", "4Gb","HD")
+
+// document.write(`
+//     <h2>Celulares gama media</h2><br>
+//     ${celular1.mobileInfo()} <br>
+//     ${celular2.mobileInfo()} <br>
+//     ${celular3.mobileInfo()} <br>
+
+//     `);document.write(`
+//         <h2>Celulares Alta gama</h2><br>
+//     ${celular4.infoAltaGama()} <br><br>
+//     ${celular5.infoAltaGama()} <br>
+
+//     `);
+
+// EJERCICIO 4
+
+class App {
+  constructor(descargas, puntuacion, peso) {
+    this.descargas = descargas;
+    this.puntuacion = puntuacion;
     this.peso = peso;
-    this.tamaño = tamaño;
-    this.resolucionDeCamara = rdc;
-    this.memoriaRam = ram;
-    this.encendido = false;
+    this.iniciada = false;
+    this.instalada = false;
   }
-  presionarBotonEncendido() {
-    if (this.encendido == false) {
-      alert("Celular prendido");
-      this.encendido = true;
-    } else {
-      alert("El celula apagado");
-      this.encendido = false;
+  abrir() {
+    if(this.iniciada == false){
+        this.iniciada = true
+        alert("Aplicacion iniciada")
     }
   }
-  reiniciar() {
-    if (this.encendido == true) {
-      alert("Celular reiniciado");
-    } else {
-      alert("El celular esta apagado");
+  
+  cerrar() {
+    if(this.iniciada == true){
+        this.iniciada = false
+        alert("Aplicacion cerrada")
     }
   }
-  tomarFoto() {
-    alert(`foto tomada en resolucion de: ${this.resolucionDeCamara}`);
+
+  instalar(){
+    if(this.instalada == false){
+        this.instalada = true
+        alert("Aplicacion instalada")
+        }
+  }
+  
+  desinstalar(){
+    if(this.instalada == true){
+        this.instalada = false
+        alert("Aplicacion desinstalada")
+        }
+  }
+  abrir() {
+    if(this.iniciada == false && this.instalada == true){
+        this.iniciada = true
+        alert("Aplicacion iniciada")
+    }
+  }
+  
+  cerrar() {
+    if(this.iniciada == true && this.instalada == true){
+        this.iniciada = false
+        alert("Aplicacion cerrada")
+    }
   }
 
-  grabarVideo() {
-    alert(`Grabando video en resolucion de: ${this.resolucionDeCamara}`);
-  }
-
-  mobileInfo() {
+  appInfo(){
     return `
-        Color: <b>${this.color}</b><br>
-        Peso: <b>${this.peso}</b><br>
-        Tamaño: <b>${this.tamaño}</b><br>
-        resolucion de camara: <b>${this.resolucionDeCamara}</b><br>
-        Memoria ram: <b>${this.memoriaRam}</b><br>
-        `;
+    Descargas: <b>${this.descargas}</b> <br>
+    Puntuacion: <b>${this.puntuacion}</b> <br>
+    Peso: <b>${this.peso}</b> <br>
+    `
   }
 }
 
-class CelularAltaGama extends Celular{
-    constructor(color, peso, tamaño, rdc, ram,rdce){
-        super(color, peso, tamaño, rdc, ram)
-        this.resolucionDeCamaraExtra = rdce
-}
-    grabarVideoLento(){
-        alert("Grabando en camara lenta")
-    }
-    reconocimientoFacial(){
-        alert("Reconociendo rostro")
-    }
 
-    infoAltaGama(){
-        return this.mobileInfo() + 
-        `Resolucion de camara extra: <b>${this.resolucionDeCamaraExtra}</b>`
-    }
+const app1 = new App("16.000 descargas","5 Estrellas","900mb" )
+const app2 = new App("170.000 descargas","4 Estrellas","800mb" )
+const app3 = new App("1.000.000 descargas","5 Estrellas","650mb" )
+const app4 = new App("40.000 descargas","4 Estrellas","900mb" )
+const app5 = new App("1.000 descargas","3 Estrellas","50mb" )
+const app6 = new App("500 descargas","2 Estrellas","400mb" )
+const app7 = new App("19.000 descargas","3 Estrellas","1200mb" )
 
-
-}
-
-
-const celular1 = new Celular("rojo", "150gr", "5'", "Hd", "1Gb");
-const celular2 = new Celular("negro", "155gr", "5.4", "Full Hd", "2GB");
-const celular3 = new Celular("blanco", "146gr", "5.9", "Full Hd", "2GB");
-
-// celular1.presionarBotonEncendido()
-// celular1.tomarFoto()
-// celular1.grabarVideo()
-// celular1.reiniciar()
-// celular1.presionarBotonEncendido()
-
-const celular4 = new CelularAltaGama("rojo", "130gr", "5.2'", "4K", "3Gb","Full HD")
-const celular5 = new CelularAltaGama("negro", "140gr", "6'", "4K", "4Gb","HD")
+// app.instalar()
+// app.abrir()
+// app.cerrar()
+// app.desinstalar()
 
 document.write(`
-    <h2>Celulares gama media</h2><br>
-    ${celular1.mobileInfo()} <br>
-    ${celular2.mobileInfo()} <br>
-    ${celular3.mobileInfo()} <br>
-
-    `);document.write(`
-        <h2>Celulares Alta gama</h2><br>
-    ${celular4.infoAltaGama()} <br><br>
-    ${celular5.infoAltaGama()} <br>
+    ${app1.appInfo()} <br>
+    ${app2.appInfo()} <br>
+    ${app3.appInfo()} <br>
+    ${app4.appInfo()} <br>
+    ${app5.appInfo()} <br>
+    ${app6.appInfo()} <br>
+    ${app7.appInfo()} <br>
     
-
-    `);
-
+    `)
